41c764436109fc7b75e331a87f5c75696e4cdfb6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-06-11 16:29:59 +0000

    hard-coded model persistence - needs refactoring!!

--- inst/container-creation/command-line-installers/ollama.sh
+++ inst/container-creation/command-line-installers/ollama.sh
@@ -5,10 +5,19 @@ set -e
 source set-container-envars
 
 # https://docs.ollama.com/linux#install
+echo "..Stopping existing ollama.service if any"
+sudo systemctl disable --now ollama.service || treu
 echo "..Installing Ollama with official installer"
 curl -fsSL https://ollama.com/install.sh | sh || true
+
+echo "..Fixing model storage owner:group"
+sudo systemctl disable --now ollama.service || true
+sudo chown --recursive ollama.ollama /usr/share/ollama
 sudo systemctl enable --now ollama.service
-ollama --version || true
+systemctl status ollama.service
+
+echo ""
+ollama list
 
 echo "** Finished Ollama **"
 echo ""
