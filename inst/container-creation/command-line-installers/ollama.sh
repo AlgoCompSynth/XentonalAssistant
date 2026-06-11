4275c2e8388555b90405716d442b0af564f739eb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-06-11 09:38:06 -0700

    typo-b-gon

--- inst/container-creation/command-line-installers/ollama.sh
+++ inst/container-creation/command-line-installers/ollama.sh
@@ -6,7 +6,7 @@ source set-container-envars
 
 # https://docs.ollama.com/linux#install
 echo "..Stopping existing ollama.service if any"
-sudo systemctl disable --now ollama.service || treu
+sudo systemctl disable --now ollama.service || true
 echo "..Installing Ollama with official installer"
 curl -fsSL https://ollama.com/install.sh | sh || true
 
